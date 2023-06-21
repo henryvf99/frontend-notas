62263c1070df904b0cdd64cde3fcfd7925aa081e
ElvisCA01 <mark.nicols@hotmail.com>
2023-06-21 00:18:03 -0500

    Añadidos: Footer, Header, Estilos Básicos y Responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {RegisterComponent} from "./register/register.component";
-import {LoginComponent} from "./login/login.component";
-import {NotasComponent} from "./notas/notas.component";
+import {RegisterComponent} from "./pages/register/register.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {NotasComponent} from "./pages/notas/notas.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { NotasComponent } from './notas/notas.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotasComponent } from './pages/notas/notas.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { HeaderComponent } from './shell/header/header.component';
+import { FooterComponent } from './shell/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     LoginComponent,
-    NotasComponent
+    NotasComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
